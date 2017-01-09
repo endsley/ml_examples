0ff0f680936f667303ac0a73acf555dda5aedb37
chieh <juliusctw@gmail.com>
2017-01-09 11:06:21 -0500

    added approximate kernel example

---
+++ approximate_kernel/approx_kernel_with_nystrom.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import numpy as np
+from sklearn.kernel_approximation import Nystroem
+import sklearn.metrics
+import time 
+
+#	This code demonstrate how using Nystrom could speed up the
+#	Calculation of a Kernel
+
+#	K(x, y) = exp(-gamma ||x-y||^2)
+#	sigma = sqrt( 1/(2*gamma) )
+#	gamma = 1/(2*sigma^2)
+
+num_of_samples = 14000
+X = np.random.random((num_of_samples,5))
+sampling_percentage = 0.05
+
+
+start_time = time.time() 
+N = Nystroem(gamma=1,n_components= int(num_of_samples*sampling_percentage))
+V = N.fit_transform(X)
+estimated_kernel = V.dot(V.T)
+print("--- Nystrom Time : %s seconds ---" % (time.time() - start_time))
+
+
+start_time = time.time() 
+real_kernel = sklearn.metrics.pairwise.rbf_kernel(X, gamma=1)
+print("--- Real Time : %s seconds ---" % (time.time() - start_time))
+
+
+
+print estimated_kernel[0:5, 0:5]
+print '\n\n'
+print real_kernel[0:5, 0:5]
+
+
+
+
