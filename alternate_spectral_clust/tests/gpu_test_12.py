81d7ec4d2d80f2b41ba8e1ea0d5cb0cb64af6b81
Chieh Wu <juliusctw@gmail.com>
2017-02-03 08:09:03 -0500

    tested a bunch of gpu tests

--- alternate_spectral_clust/tests/gpu_test_12.py
+++ alternate_spectral_clust/tests/gpu_test_12.py
@@ -2,7 +2,7 @@
 
 import sys
 sys.path.append('./lib')
-from alt_spectral_clust import *
+from alt_spectral_clust_gpu import *
 import numpy as np
 #from io import StringIO   # StringIO behaves like a file object
 from numpy import genfromtxt
@@ -109,9 +109,6 @@ if True:	# run alternative clustering
 
 
 
-
-
-
 	txt = '\tLabel against alternative : ' + str(against_alternative) + '\n'
 	txt += '\tLabel against truth : ' + str(against_truth) + '\n'
 	txt += '\tAlternative HSIC % diff from random : ' + str(percent_diff) + '\n'
@@ -156,49 +153,6 @@ if False:	# save or load db to and from a pickle file
 	#db['debug_debug_Wchange'] = plot_info['debug_debug_Wchange']
 
 
-
-if True:	# plot the W convergence results
-	X = db['data']
-	plt.figure(2)
-	
-	plt.suptitle('facial.csv',fontsize=24)
-	plt.subplot(311)
-	inc = 0
-	for costs in db['debug_costVal']: 
-		xAxis = np.array(range(len(costs))) + inc; 
-		inc = np.amax(xAxis)
-		plt.plot(xAxis, costs, 'b')
-		plt.plot(inc, costs[-1], 'bo', markersize=10)
-		plt.title('Cost vs w iteration, each dot is U update')
-		plt.xlabel('w iteration')
-		plt.ylabel('cost')
-		
-	plt.subplot(312)
-	inc = 0
-	for gradient in db['debug_gradient']: 
-		xAxis = np.array(range(len(gradient))) + inc; 
-		inc = np.amax(xAxis)
-		plt.plot(xAxis, gradient, 'b')
-		plt.plot(inc, gradient[-1], 'bo', markersize=10)
-		plt.title('Gradient vs w iteration, each dot is U update')
-		plt.xlabel('w iteration')
-		plt.ylabel('gradient')
-
-
-	plt.subplot(313)
-	inc = 0
-	for wchange in db['debug_debug_Wchange']: 
-		xAxis = np.array(range(len(wchange))) + inc; 
-		inc = np.amax(xAxis)
-		plt.plot(xAxis, wchange, 'b')
-		plt.plot(inc, wchange[-1], 'bo', markersize=10)
-		plt.title('|w_old - w_new|/|w| vs w iteration, each dot is U update')
-		plt.xlabel('w iteration')
-		plt.ylabel('|w_old - w_new|/|w| ')
-
-	plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=0.4)
-	plt.subplots_adjust(top=0.85)
-	plt.show()
 
 
 import pdb; pdb.set_trace()
