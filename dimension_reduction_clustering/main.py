7a48bf2663a1cd373f539a48aa2525ce15368c9d
chieh <juliusctw@gmail.com>
2017-03-18 20:01:15 -0400

    revised DRC folder order

---
+++ dimension_reduction_clustering/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('./tests')
+
+
+
+import test_1.py		# 2 gaussian with noise data ( linear dimensionality reduction, gaussian spectral )
+import test_2.py		# moon dataset ( linear dimensionality reduction, gaussian spectral )
