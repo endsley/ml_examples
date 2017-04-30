963b3e139832ab10145424a1617c6b070dd43655
chieh <juliusctw@gmail.com>
2017-04-30 14:18:05 -0400

    added plot_bar

--- alternate_spectral_clust/paper/plot_bar_speed.py
+++ alternate_spectral_clust/paper/plot_bar_speed.py
@@ -17,25 +17,13 @@ ISM = np.log(np.array([0.01, 1.7, 3.6, 0.35, 0.78, 18.56]))
 GPU = np.log(run_times*np.array([0.023, 12.67, 6.49, 3.8, 134, 600]))
 Orthogonal = np.log(run_times*np.array([0.07, 2.34, 3.4, 0.3, 0.68, 16.73]))
 
-#minMag = np.min(np.hstack((KDAC, ISM))) - 1
-#maxMag = np.max(np.hstack((KDAC, ISM))) + 1
-##print 'Min : ' , np.min(np.hstack((KDAC, ISM))) - 1
-#
-#
-#KDAC = KDAC + np.abs(minMag)
-#ISM = ISM + np.abs(minMag)
-
-
-
 
 N = len(KDAC)
-#men_std = (2, 3, 4, 1, 2)
 
 ind = np.arange(N)  # the x locations for the groups
 width = 0.20       # the width of the bars
 
 plt.subplot(122)
-#plt.bar(ind, KDAC, width, color='w', hatch="/" )
 rects1 = plt.bar(ind, KDAC, width, color='w', hatch="/" )
 rects2 = plt.bar(ind + width, GPU, width, color='w', hatch='*')
 rects3 = plt.bar(ind + 2*width, Orthogonal, width, color='w', hatch='x')
