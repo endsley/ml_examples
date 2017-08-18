69a23efa136ee735f7efe9f27ba3def7a92a9bed
chieh <juliusctw@gmail.com>
2017-08-18 11:21:18 -0400

    added the normalized random fourier feature

--- Random_Fourier_Features/Random_Fourier_Features.py
+++ Random_Fourier_Features/Random_Fourier_Features.py
@@ -5,6 +5,7 @@
 import numpy as np
 import sklearn.metrics
 import numpy.matlib
+from sklearn.preprocessing import normalize
 
 #	K(x, y) = exp(-gamma ||x-y||^2)
 #	sigma = sqrt( 1/(2*gamma) )
@@ -15,22 +16,47 @@ np.set_printoptions(threshold=np.nan)
 np.set_printoptions(linewidth=300)
 np.set_printoptions(suppress=True)
 
+def RFF(X, nrmlize, m):
+	if nrmlize:
+		X = normalize(X, norm='l2', axis=1)
+	
+	n = X.shape[0]
+	d = X.shape[1]
+	rbk = sklearn.metrics.pairwise.rbf_kernel(X, gamma=0.5)
+	#print rbk
+	
+	u = np.random.randn(d, m)
+	b = np.random.rand(1, m)
+	b = np.matlib.repmat(b, n, 1)*2*np.pi
+	
+	y = np.cos(X.dot(u) + b)
+	K = y.dot(y.T)*2/m
+	error = np.linalg.norm(rbk - K)
+	#print K , '\n'
+	#print 'Error from RFF : ' , error , '\n\n'
 
+	return error
+
+
+
+#	This example shows that normalized RFF can be approximated with lower variance
+#	The purpose of the normalized version is NOT to estimate the original kernel
+#	but a proposal of another kernel that when estimated has a lower variance.
+m = 100
 X = np.array([[1,2],[2,3],[1,1],[1,3]], dtype='f')	 
-n = X.shape[0]
-d = X.shape[1]
-rbk = sklearn.metrics.pairwise.rbf_kernel(X, gamma=0.5)
-print rbk
+
+RFF_error = []
+for p in range(1000):
+	RFF_error.append(RFF(X, False, m))
+
+print np.mean(RFF_error)
+
+RFF_error = []
+for p in range(1000):
+	RFF_error.append(RFF(X, True, m))
+
+print np.mean(RFF_error)
 
 
-m = 1000
-u = np.random.randn(d, m)
-b = np.random.rand(1, m)
-b = np.matlib.repmat(b, n, 1)*2*np.pi
 
-y = np.cos(X.dot(u) + b)
-K = y.dot(y.T)*2/m
-error = np.linalg.norm(rbk - K)
-print K , '\n'
-print 'Error : ' , error
 import pdb; pdb.set_trace()
