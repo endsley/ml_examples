0edc42cf6171aae603e442f0d00ba9ecc32831e1
chieh <juliusctw@gmail.com>
2017-09-07 19:59:25 -0400

    added coordinate wise power method but it doesn't currently work

---
+++ coordinate_wise_power_method/CPM.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#	Assume symmetric matrix
+
+import numpy as np
+
+
+def CPM(A):		#	A is a symmetric np array
+	n = A.shape[0]	
+	x = np.random.randn(n,1)
+	x = x/np.linalg.norm(x)
+	z = A.dot(x)
+	c = np.absolute(z - x)
+	loop = True
+
+
+	while loop: 
+		i = np.argmax(c)
+	
+		y = np.copy(x)
+		z_orig = np.copy(z)
+		y[i] = (z_orig/(x.T.dot(z_orig)))[i]
+
+		xi = x[i]
+		
+		z = z_orig + (A[:, np.argmax(c)]*(y[i] - x[i])).reshape(n,1)
+
+		yn = np.linalg.norm(y)
+		z = z/yn
+		x = y/yn
+
+		y[i] = xi		# turn y into original x
+	
+		c = x - z/(y.T.dot(z_orig))
+
+		if(np.sum(np.absolute(x - y)) < 0.00000001): 
+			print A.dot(x)
+			print A.dot(x)/x
+			print '--------'
+			break;
+
+	return x
+
+if __name__ == "__main__":
+	A = np.random.randn(3,3)
+	A = A + A.T
+	print CPM(A) , '\n'
+
+	[S,V] = np.linalg.eig(A)
+	print V, '\n\n'
+	print S 
+	import pdb; pdb.set_trace()
