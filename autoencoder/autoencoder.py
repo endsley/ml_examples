c239dfe1f593920bd2ce795030838a6addb43b38
chieh <juliusctw@gmail.com>
2017-11-16 12:06:58 -0500

    dfkjkjdf

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import torch
+import os
 from torch.autograd import Variable
 import numpy as np
 import collections
+import pickle
 
 class autoencoder():
 	def __init__(self, X, n_encoding_layers, D_out):	#D_out is the Dimension of bottleneck
@@ -15,45 +17,63 @@ class autoencoder():
 		tmpX = torch.from_numpy(X)
 		self.X = Variable(tmpX.type(self.dtype), requires_grad=False)
 		self.loss_func = torch.nn.MSELoss(size_average=False)
+		self.loss_fn = torch.nn.MSELoss(size_average=False)
 
-		nodeDiff = self.D_in - D_out
-		layer_decrease = int(nodeDiff/float(n_encoding_layers))
-		#layer_decrease = 0
+		
+		[self.encoder, encodeElements] = self.build_encoder(n_encoding_layers)
+		[self.decoder, decodeElements] = self.build_decoder(n_encoding_layers)
+
+		self.jointParams = [p for p in self.encoder.parameters()] + [p for p in self.decoder.parameters()]
+
+
+	def build_decoder(self, n_encoding_layers):
 		layer_list = []
+		nodeDiff = self.D_in - self.D_out
+		layer_decrease = int(nodeDiff/float(n_encoding_layers))
 
 		l = self.D_in
 		layer_index = 0
 		for i in range(n_encoding_layers):
 			l2 = l - layer_decrease
 
-			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(l, l2, bias=True))
+			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(l2, l, bias=True))
 			layer_index += 1
 			layer_list.append(linLayer)
 
-			Relayer = ('Relu:' + str(layer_index), torch.nn.ReLU())
-			layer_index += 1
-			layer_list.append(Relayer)
+			if i < (n_encoding_layers-1):
+				Relayer = ('Relu:' + str(layer_index), torch.nn.ReLU())
+				layer_index += 1
+				layer_list.append(Relayer)
 
 			l = l2
 
-		if l > D_out:
-			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(l, D_out, bias=True))
-			layer_index += 1
-			layer_list.append(linLayer)
+		if l > self.D_out:
 			Relayer = ('Relu:' + str(layer_index), torch.nn.ReLU())
 			layer_index += 1
 			layer_list.append(Relayer)
 
-			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(D_out, l, bias=True))
+			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(self.D_out, l, bias=True))
 			layer_index += 1
 			layer_list.append(linLayer)
-			Relayer = ('Relu:' + str(layer_index), torch.nn.ReLU())
-			layer_index += 1
-			layer_list.append(Relayer)
 
 
+		layer_list = layer_list[::-1]
+		OD = collections.OrderedDict(layer_list)
+		decoder = torch.nn.Sequential(OD)
+	
+		return [decoder, layer_list] 
+
+
+
+	def build_encoder(self, n_encoding_layers):
+		layer_list = []
+		nodeDiff = self.D_in - self.D_out
+		layer_decrease = int(nodeDiff/float(n_encoding_layers))
+
+		l = self.D_in
+		layer_index = 0
 		for i in range(n_encoding_layers):
-			l2 = l + layer_decrease
+			l2 = l - layer_decrease
 
 			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(l, l2, bias=True))
 			layer_index += 1
@@ -66,9 +86,20 @@ class autoencoder():
 
 			l = l2
 
+		if l > self.D_out:
+			Relayer = ('Relu:' + str(layer_index), torch.nn.ReLU())
+			layer_index += 1
+			layer_list.append(Relayer)
+
+			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(l, self.D_out, bias=True))
+			layer_index += 1
+			layer_list.append(linLayer)
+
+
 		OD = collections.OrderedDict(layer_list)
-		self.NN = torch.nn.Sequential(OD)
-		self.layer_list = layer_list
+		encoder = torch.nn.Sequential(OD)
+		
+		return [encoder, layer_list]
 
 
 	def show_autoencoder_layers(self):
@@ -76,48 +107,73 @@ class autoencoder():
 
 
 	def get_optimizer(self, learning_rate, opt='Adam'):
-		if opt == 'Adam': return torch.optim.Adam(self.NN.parameters(), lr=learning_rate)
-		if opt == 'Adamax': return torch.optim.Adamax(model.parameters(), lr=learning_rate)
-		if opt == 'ASGD': return torch.optim.ASGD(model.parameters(), lr=learning_rate)
-		if opt == 'SGD': return torch.optim.SGD(model.parameters(), lr=learning_rate)
+		if opt == 'Adam': return torch.optim.Adam(self.jointParams, lr=learning_rate)
+		#if opt == 'Adamax': return torch.optim.Adamax(model.parameters(), lr=learning_rate)
+		#if opt == 'ASGD': return torch.optim.ASGD(model.parameters(), lr=learning_rate)
+		#if opt == 'SGD': return torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-	def optimize(self):
-		loss_fn = torch.nn.MSELoss(size_average=False)
-		learning_rate = 1e-6
-		optimizer = self.get_optimizer(learning_rate, opt='Adam')
-		
+	def forward_autoencoder(self):
+		encode_X = self.encoder(self.X)
+		y_pred = self.decoder(encode_X)
+		return self.loss_fn(y_pred, self.X)
 
-		y_pred = self.NN(self.X)
-		loss = loss_fn(y_pred, self.X)
+
+	def optimize_autoencoder(self):
+		lr = 1e-2
+		optimizer = self.get_optimizer(lr, opt='Adam')
+
+		loss = self.forward_autoencoder()
 
 		for t in range(500000):
-			#self.NN.zero_grad()
 			optimizer.zero_grad()
 			loss.backward()
 			optimizer.step()
 		
-			#for param in self.NN.parameters():
-			#	param.data -= learning_rate * param.grad.data
-		
-			print loss.data[0]
 
-			y_pred = self.NN(self.X)
-			new_loss = loss_fn(y_pred, self.X)
-			lossDiff = np.absolute(loss.data[0] - new_loss.data[0])
+			new_loss = self.forward_autoencoder()
+			lossDiff = loss.data[0] - new_loss.data[0]
 
-			#if(lossDiff < 0.0000000000001): break;
+			if lossDiff <= 0.00000000001:
+				lr = lr*0.7
+				optimizer = self.get_optimizer(lr, opt='Adam')
+	
+			if lr < 0.0000000001: break;
 			if new_loss.data[0] < 0.01: break;
 			loss = new_loss
 
 
+		return loss
+		#encode_X = self.encoder(self.X)
+		#print encode_X
+
+	def save_result(self):
+		result = { "lion": "yellow", "kitty": "red" }
+		pickle.dump( favorite_color, open( "save.p", "wb" ) )
 
 
+def train():
+	x = np.random.randn(30, 5)
+	AE = autoencoder(x, 2, 2)
+	loss = AE.optimize_autoencoder()
+	
+	if os.path.exists('results.pk'):
+		res = pickle.load(open('results.pk','rb'))
+		if loss.data[0] < res['loss']:
+			res['loss'] = loss.data[0]
+			res['autoencoder'] = AE
+			print 'Lowest loss : ' , res['loss']
+	else:
+		res = {}
+		res['loss'] = loss.data[0]
+		res['autoencoder'] = AE
+	
+	pickle.dump(res, open('results.pk','wb'))
 
-#x = np.array([[1,0,0],[1,0,0],[0,1,0],[0,1,0],[0,0,1],[0,0,1]])
-#y = np.array([[0.3],[0.3],[0.6],[0.6],[1],[1]])
-x = np.random.randn(30, 20)
+def load():
+	if os.path.exists('results.pk'):
+		res = pickle.load(open('results.pk','rb'))
 
-#x = np.ones((10,9))
-AE = autoencoder(x, 4, 2)
-AE.optimize()
-
+		print res['loss']
+		print res['autoencoder']
+		
+load()
