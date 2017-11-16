3ab35818c0b38b10414c403364a7ab82f13d5fd8
chieh <juliusctw@gmail.com>
2017-11-15 21:59:08 -0500

    dfkjkjdf

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -17,14 +17,43 @@ class autoencoder():
 		self.loss_func = torch.nn.MSELoss(size_average=False)
 
 		nodeDiff = self.D_in - D_out
-		#layer_decrease = int(nodeDiff/float(n_encoding_layers))
-		layer_decrease = 0
+		layer_decrease = int(nodeDiff/float(n_encoding_layers))
+		#layer_decrease = 0
 		layer_list = []
 
 		l = self.D_in
 		layer_index = 0
 		for i in range(n_encoding_layers):
 			l2 = l - layer_decrease
+
+			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(l, l2, bias=True))
+			layer_index += 1
+			layer_list.append(linLayer)
+
+			Relayer = ('Relu:' + str(layer_index), torch.nn.ReLU())
+			layer_index += 1
+			layer_list.append(Relayer)
+
+			l = l2
+
+		if l > D_out:
+			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(l, D_out, bias=True))
+			layer_index += 1
+			layer_list.append(linLayer)
+			Relayer = ('Relu:' + str(layer_index), torch.nn.ReLU())
+			layer_index += 1
+			layer_list.append(Relayer)
+
+			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(D_out, l, bias=True))
+			layer_index += 1
+			layer_list.append(linLayer)
+			Relayer = ('Relu:' + str(layer_index), torch.nn.ReLU())
+			layer_index += 1
+			layer_list.append(Relayer)
+
+
+		for i in range(n_encoding_layers):
+			l2 = l + layer_decrease
 
 			linLayer = ('Linear:' + str(layer_index), torch.nn.Linear(l, l2, bias=True))
 			layer_index += 1
@@ -37,112 +66,58 @@ class autoencoder():
 
 			l = l2
 
-		print layer_list
-		import pdb; pdb.set_trace()
 		OD = collections.OrderedDict(layer_list)
 		self.NN = torch.nn.Sequential(OD)
+		self.layer_list = layer_list
 
 
+	def show_autoencoder_layers(self):
+		for m in self.layer_list: print m
 
-	def forward(self):
-		h = self.NN(self.X)
-		loss = self.loss_func(h, self.X)
-		#loss = (h - self.X).pow(2).sum()
-		return loss
 
-	def step_forward(self, lr):
-		cost = self.forward()
-		self.NN.zero_grad()
-		cost.backward()
-
-		for param in self.NN.parameters():
-			param.data -= lr * param.grad.data
-			print 'Norm : ' , np.linalg.norm(param.grad.data.numpy())
-
-		new_cost = self.forward()
-		return [cost, new_cost]
-
-	def step_back(self, lr):
-		for param in self.NN.parameters():
-			param.data += lr * param.grad.data
+	def get_optimizer(self, learning_rate, opt='Adam'):
+		if opt == 'Adam': return torch.optim.Adam(self.NN.parameters(), lr=learning_rate)
+		if opt == 'Adamax': return torch.optim.Adamax(model.parameters(), lr=learning_rate)
+		if opt == 'ASGD': return torch.optim.ASGD(model.parameters(), lr=learning_rate)
+		if opt == 'SGD': return torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 	def optimize(self):
 		loss_fn = torch.nn.MSELoss(size_average=False)
+		learning_rate = 1e-6
+		optimizer = self.get_optimizer(learning_rate, opt='Adam')
 		
-		learning_rate = 1e-4
+
+		y_pred = self.NN(self.X)
+		loss = loss_fn(y_pred, self.X)
+
 		for t in range(500000):
-			y_pred = self.NN(self.X)
-			loss = loss_fn(y_pred, self.X)
-			print(t, loss.data[0])
-			self.NN.zero_grad()
+			#self.NN.zero_grad()
+			optimizer.zero_grad()
 			loss.backward()
+			optimizer.step()
 		
-			for param in self.NN.parameters():
-				param.data -= learning_rate * param.grad.data
+			#for param in self.NN.parameters():
+			#	param.data -= learning_rate * param.grad.data
 		
-			if loss.data[0] < 0.01:
-				print y_pred
-				break;
+			print loss.data[0]
+
+			y_pred = self.NN(self.X)
+			new_loss = loss_fn(y_pred, self.X)
+			lossDiff = np.absolute(loss.data[0] - new_loss.data[0])
+
+			#if(lossDiff < 0.0000000000001): break;
+			if new_loss.data[0] < 0.01: break;
+			loss = new_loss
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-#		lr = 1
-#
-#		#self.print_W_shapes()
-#
-#		while True:
-#			[loss, loss_after] = self.step_forward(lr)
-#
-#			if loss_after.data[0] > loss.data[0]: 
-#				self.step_back(lr)
-#				lr = lr * 0.6
-#			else:
-#				lr = lr * 1.15
-#
-#			print loss.data[0], lr
-#
-#			if np.absolute(loss_after.data[0] - loss.data[0]) < 0.000000001: break;
-#
-#
-#		import pdb; pdb.set_trace()			
-
-
-
-
-
-
-
-
-
-
-
-
-			#loss = self.forward()
-			#loss.backward()
-
-			#print loss.data[0]
-			#for i, j in self.W.items():
-			#	self.W[i].data -= lr * self.W[i].grad.data
-			#	self.W[i].grad.data.zero_()
-
-x = np.array([[1,0,0],[1,0,0],[0,1,0],[0,1,0],[0,0,1],[0,0,1]])
+#x = np.array([[1,0,0],[1,0,0],[0,1,0],[0,1,0],[0,0,1],[0,0,1]])
 #y = np.array([[0.3],[0.3],[0.6],[0.6],[1],[1]])
-#x = np.random.randn(20,8)
+x = np.random.randn(30, 20)
 
 #x = np.ones((10,9))
-AE = autoencoder(x,2, 2)
+AE = autoencoder(x, 4, 2)
 AE.optimize()
 
