6774b465da42e2051a020f51b6283aa4aba6bb94
“chieh-neu” <juliusctw@gmail.com>
2019-02-01 09:49:23 -0500

    updated identity net

--- pytorch_examples/identity_net/identity_net.py
+++ pytorch_examples/identity_net/identity_net.py
@@ -22,29 +22,27 @@ class identity_net(torch.nn.Module):
 		
 
 		self.l0 = torch.nn.Linear(self.input_size, self.net_width , bias=True)
-		for l in range(self.net_depth-2):
+		for l in range(self.net_depth-1):
 			lr = 'self.l' + str(l+1) + ' = torch.nn.Linear(' + str(self.net_width) + ', ' + str(self.net_width) + ' , bias=True)'
 			exec(lr)
 
-		lr = 'self.l' + str(self.net_depth-1) + ' = torch.nn.Linear(' + str(self.net_width) + ', ' + str(self.input_size) + ' , bias=True)'
+		lr = 'self.l' + str(self.net_depth) + ' = torch.nn.Linear(' + str(self.net_width) + ', ' + str(self.input_size) + ' , bias=True)'
 		exec(lr)
-		exec('self.l' + str(self.net_depth-1) + '.activation = "none"')		#softmax, relu, tanh, sigmoid, none
+		exec('self.l' + str(self.net_depth) + '.activation = "none"')		#softmax, relu, tanh, sigmoid, none
 
 
 		if add_decoder:
-			lr = 'self.l' + str(self.net_depth) + ' = torch.nn.Linear(' + str(self.input_size) + ', ' + str(self.net_width) + ' , bias=True)'
+			lr = 'self.l' + str(self.net_depth+1) + ' = torch.nn.Linear(' + str(self.input_size) + ', ' + str(self.net_width) + ' , bias=True)'
 			exec(lr)
 
-			for l in range(self.net_depth-2):
-				lr = 'self.l' + str(self.net_depth+1+l) + ' = torch.nn.Linear(' + str(self.net_width) + ', ' + str(self.net_width) + ' , bias=True)'
+			for l in range(self.net_depth-1):
+				lr = 'self.l' + str(self.net_depth+2+l) + ' = torch.nn.Linear(' + str(self.net_width) + ', ' + str(self.net_width) + ' , bias=True)'
 				exec(lr)
 	
-			lr = 'self.l' + str(2*self.net_depth-1) + ' = torch.nn.Linear(' + str(self.net_width) + ', ' + str(self.input_size) + ' , bias=True)'
+
+			lr = 'self.l' + str(2*self.net_depth+1) + ' = torch.nn.Linear(' + str(self.net_width) + ', ' + str(self.input_size) + ' , bias=True)'
 			exec(lr)
-			exec('self.l' + str(2*self.net_depth-1) + '.activation = "none"')		#softmax, relu, tanh, sigmoid, none
-
-
-
+			exec('self.l' + str(2*self.net_depth+1) + '.activation = "none"')		#softmax, relu, tanh, sigmoid, none
 
 
 		self.output_network()
@@ -76,22 +74,23 @@ class identity_net(torch.nn.Module):
 		original_column = np.copy(one_column)
 
 		eyeMatrix = torch.eye(self.net_width)
+		eyeMatrix = Variable(eyeMatrix.type(self.db['dataType']), requires_grad=False)		
 
 		for i in range(self.input_size-1):
 			one_column = np.roll(one_column, 2*self.width_scale)
 			original_column = np.hstack((original_column, one_column))
 
-		original_column = (1.0/self.width_scale)*original_column
 		original_column = torch.tensor(original_column)
+		original_column = Variable(original_column.type(self.db['dataType']), requires_grad=False)		
+
 
 		for i, param in enumerate(self.parameters()):
-			#print(i)
 			if len(param.data.shape) == 1:
 				param.data = torch.zeros(param.data.size())
 			else:
-				if i == 0: 
-					param.data = original_column
-				elif i == self.net_depth*2-2:
+				if param.data.shape[1] == self.input_size:
+					param.data = (1.0/self.width_scale)*original_column
+				elif param.data.shape[0] == self.input_size:
 					param.data = original_column.t()
 				else:
 					param.data = eyeMatrix
@@ -100,17 +99,10 @@ class identity_net(torch.nn.Module):
 		#	print(param.data)
 
 
-			#if(len(param.data.numpy().shape)) > 1:
-			#	torch.nn.init.kaiming_normal_(param.data , a=0, mode='fan_in')	
-			#else:
-			#	pass
-			#	#param.data = torch.zeros(param.data.size())
-
-		#self.num_of_linear_layers = 0
-		#for m in self.children():
-		#	if type(m) == torch.nn.Linear:
-		#		self.num_of_linear_layers += 1
-
+		self.num_of_linear_layers = 0
+		for m in self.children():
+			if type(m) == torch.nn.Linear:
+				self.num_of_linear_layers += 1
 
 		#	If using L21 regularizer
 		#hsic_cost = HSIC_AE_objective(self, db)
@@ -136,38 +128,26 @@ class identity_net(torch.nn.Module):
 		self.y0 = y0
 
 		for m, layer in enumerate(self.children(),0):
-			print(m , layer)
+			if m == self.net_depth:
+				cmd = 'self.y' + str(m+1) + ' = self.y_pred = self.l' + str(m) + '(self.y' + str(m) + ')'
+				exec(cmd)
+				#print(m , layer)
+				#print(cmd)
+				#import pdb; pdb.set_trace()
+			elif m == self.net_depth*2+1:
+				cmd = 'self.y' + str(m+1) + ' = self.fx = self.l' + str(m) + '(self.y' + str(m) + ')'
+				exec(cmd)
+				#print(m , layer)
+				#print(cmd)
+				#import pdb; pdb.set_trace()
+			else:
+				cmd = 'self.y' + str(m+1) + ' = F.relu(self.l' + str(m) + '(self.y' + str(m) + '))'
+				exec(cmd)
+				#print(m , layer)
+				#print(cmd)
+				#import pdb; pdb.set_trace()
 
-			cmd = 'self.y' + str(m+1) + ' = F.relu(self.l' + str(m) + '(self.y' + str(m) + '))'
-			print(cmd)
-			#cmd2 = var + '= F.dropout(' + var + ', training=self.training)'
-			#exec(cmd)
 
-			import pdb; pdb.set_trace()
-
-
-
-#			if m == self.net_depth*2:
-#				cmd = 'self.y_pred = self.l' + str(m) + '(y' + str(m-1) + ')'
-#				exec(cmd)
-#				break;
-#			elif m == self.net_depth:
-#				if self.add_decoder:
-#					var = 'y' + str(m)
-#					cmd = var + ' = self.l' + str(m) + '(y' + str(m-1) + ')'
-#					exec(cmd)
-#				else:
-#					cmd = 'self.y_pred = self.l' + str(m) + '(y' + str(m-1) + ')'
-#					exec(cmd)
-#					return [self.y_pred, self.y_pred]
-#
-#			else:
-#				cmd = 'self.y' + str(m) + ' = F.relu(self.l' + str(m) + '(y' + str(m-1) + '))'
-#				#cmd2 = var + '= F.dropout(' + var + ', training=self.training)'
-#				exec(cmd)
-#				#exec(cmd2)
-
-		
-		exec('self.fx = y' + str(self.net_depth))
 		return [self.y_pred, self.fx]
+
 
--- pytorch_examples/identity_net/test_net.py
+++ pytorch_examples/identity_net/test_net.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
 from identity_net import *
+import warnings
+
+
+
+np.set_printoptions(precision=4)
+np.set_printoptions(threshold=np.nan)
+np.set_printoptions(linewidth=300)
+np.set_printoptions(suppress=True)
+warnings.filterwarnings("ignore")
+torch.set_printoptions(precision=3, linewidth=300)
+
+
 
 X = torch.randn(5,3)
 
@@ -8,7 +20,7 @@ X = torch.randn(5,3)
 
 db = {}
 db['net_input_size'] = X.shape[1]
-db['net_depth'] = 4
+db['net_depth'] = 2
 db['width_scale'] = 2
 db['cuda'] = False
 
@@ -16,6 +28,9 @@ if(db['cuda']): db['dataType'] = torch.cuda.FloatTensor
 else: db['dataType'] = torch.FloatTensor				
 db['X_var'] = Variable(X.type(db['dataType']), requires_grad=False)
 
-IN = identity_net(db, False)
-xout = IN(db['X_var'])
+IN = identity_net(db, True)
+[y_pred, auto_out] = IN(db['X_var'])
+print(X)
+print(y_pred)
+print(auto_out)
 import pdb; pdb.set_trace()
