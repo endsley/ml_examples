79e9028bcc3181cfc4eada7a3d7a8efbbfb19c3a
chieh <juliusctw@gmail.com>
2019-03-20 18:28:49 -0400

    working version

--- STSC/STSC.py
+++ STSC/STSC.py
@@ -52,7 +52,7 @@ def get_k_nearest_samples(X, x):
 	print(result)
 
 
-def cluster_plot(X, allocation):
+def cluster_plot(X, allocation, all_radius=None):
 	cmap = ['b', 'g', 'r', 'c', 'm', 'y','k']
 
 	labels = np.unique(allocation)
@@ -62,6 +62,11 @@ def cluster_plot(X, allocation):
 	for i, j in enumerate(labels):
 		subX = X[allocation == labels[i]]
 		plt.plot(subX[:,0], subX[:,1], cmap[i] + '.')
+		if all_radius is not None:
+			for p in range(X.shape[0]):
+				if np.random.rand() > 0.10:
+					circle = plt.Circle((X[p,:][0],X[p,:][1]), radius=all_radius[p], fill=None) #, color=cmap[i]
+					plt.gca().add_patch(circle)
 
 
 	plt.xlabel('x')
@@ -69,21 +74,45 @@ def cluster_plot(X, allocation):
 	plt.title('Clustering Results')
 	plt.show()
 
-
 def STSC(X, k):
+	n = X.shape[0]
+	D = sklearn.metrics.pairwise.pairwise_distances(X, metric='euclidean', n_jobs=1)
+	sD = np.sort(D, axis=1)
+	σ_list = np.reshape(sD[:,7], (n, 1))
+	#σ_list = np.reshape(sD[:,20], (n, 1))
+	σ_list[σ_list == 0] = 10
+	
+
+	σ2 = σ_list.dot(σ_list.T)
+
+	K = np.exp(-(D*D)/σ2)
+	np.fill_diagonal(K,0)
+
+	D_inv = 1.0/np.sqrt(np.sum(K, axis=1))
+	Dv = np.outer(D_inv, D_inv)
+	DKD = Dv*K
+
+	[U, U_normalized] = L_to_U(DKD, k)
+	allocation = KMeans(k).fit_predict(U_normalized)
+
+	result = {}
+	result['allocation'] = allocation
+	result['σ_list'] = σ_list
+	return result
+
+
+
+
+def STSC_σ(X, k):
 	n = X.shape[0]
 	if n < 50:
 		num_of_samples = n
+#	elif n/(2*k) < 50 and n/(2*k) > 10:
+#		num_of_samples = int(n/(2*k))
 	else:
 		num_of_samples = 50
-
-	#elif n/(k) > 50:
-	#	num_of_samples = 50
-	#
-	#elif n/(k) < 10:
-	#	num_of_samples = 10
-	#else:
-	#	num_of_samples = int(n/(4*k))
+	
+	print(num_of_samples)
 
 	unique_X = np.unique(X, axis=0)
 	neigh = NearestNeighbors(num_of_samples)
@@ -94,7 +123,6 @@ def STSC(X, k):
 	dis_inv = 1/dis[:,1:]
 	idx = idx[:,1:]
 	
-
 	total_dis = np.sum(dis_inv, axis=1)
 	total_dis = np.reshape(total_dis,(n, 1))
 	total_dis = np.matlib.repmat(total_dis, 1, num_of_samples-1)
@@ -119,12 +147,7 @@ def STSC(X, k):
 
 		result_store_dictionary[str(X[i,:])] = σ
 
-	#print(σ_list)
-	#import pdb; pdb.set_trace()
 	σ2 = σ_list.dot(σ_list.T)
-	
-
-
 
 	#n = X.shape[0]
 	D = sklearn.metrics.pairwise.pairwise_distances(X, metric='euclidean', n_jobs=1)
@@ -152,7 +175,12 @@ def STSC(X, k):
 
 	[U, U_normalized] = L_to_U(DKD, k)
 	allocation = KMeans(k).fit_predict(U_normalized)
-	return allocation
+
+	result = {}
+	result['allocation'] = allocation
+	result['σ_list'] = σ_list
+
+	return result
 
 
 
@@ -165,40 +193,45 @@ def STSC(X, k):
 #X = genfromtxt('../dataset/four_lines.csv', delimiter=','); k = 4
 
 #X = preprocessing.scale(X)
-#
-#labels = STSC(X,k)
-#cluster_plot(X, labels)
+#result = STSC(X,k)
+#result = STSC_σ(X,k)
+
+#labels = spectral(X, 1.0, k)
+#cluster_plot(X, result['allocation'], result['σ_list'])
+#cluster_plot(X, result['allocation'])
 
 
 
-
+#----------------------------------------------------------------------
 
 
 #X = genfromtxt('../dataset/facial_85.csv', delimiter=','); k = 20
 #Y = genfromtxt('../dataset/facial_true_labels_624x960.csv', delimiter=','); 
-#X = genfromtxt('../dataset/breast-cancer.csv', delimiter=','); k = 2
-#Y = genfromtxt('../dataset/breast-cancer-labels.csv', delimiter=',')	# k = 2
-#X = genfromtxt('../dataset/wine.csv', delimiter=','); k = 3
-#Y = genfromtxt('../dataset/wine_label.csv', delimiter=',')	# k = 2
-
-X = preprocessing.scale(X)
-labels = STSC(X,k)
-#labels = spectral(X, 9.0, 20)
-
-print(labels)
-nmi = normalized_mutual_info_score(labels, Y)
-print(nmi)
-
-
-
-
-
-
-#samples = [[0, 0, 2], [1, 0, 1], [0, 0, 1], [1,1,1]]
+##X = genfromtxt('../dataset/breast-cancer.csv', delimiter=','); k = 2
+##Y = genfromtxt('../dataset/breast-cancer-labels.csv', delimiter=',')	# k = 2
+##X = genfromtxt('../dataset/wine.csv', delimiter=','); k = 3
+##Y = genfromtxt('../dataset/wine_label.csv', delimiter=',')	# k = 2
 #
-#neigh = NearestNeighbors(2)
-#neigh.fit(samples)
-#result = neigh.kneighbors([[1, 1, 1.3], [0, 0, 1.9]], 2, return_distance=False)
+#X = preprocessing.scale(X)
+#result = STSC_σ(X,k)
+##result = STSC(X,k)
+##labels = spectral(X, 9.0, 20)
 #
-#print(result)
+#
+#nmi = normalized_mutual_info_score(result['allocation'], Y)
+#print(nmi)
+
+
+
+#----------------------------------------------------------------------
+
+#n = 1000
+x1 = np.random.randn(6,2) + np.array([4,4]); k = 2
+x2 = np.random.randn(6,2) + np.array([-4,-4])
+X = np.vstack((x1,x2))
+
+#result = STSC_σ(X,k)
+result = STSC(X,k)
+
+cluster_plot(X, result['allocation'], result['σ_list'])
 
