c897e598480f6be5535ccf7680838dee88f35d33
endsley <juliusctw@gmail.com>
2019-03-20 10:16:19 -0400

    get a working version of STSC

--- STSC/STSC.py
+++ STSC/STSC.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+import numpy.matlib
 import matplotlib.pyplot as plt
 import sklearn.metrics
 import sklearn.metrics
@@ -71,25 +72,62 @@ def cluster_plot(X, allocation):
 
 def STSC(X, k):
 	n = X.shape[0]
-	if n/(4*k) > 50:
-		num_of_samples = 50
-	elif n/(4*k) < 2:
-		num_of_samples = 3
+	if n < 50:
+		num_of_samples = n
 	else:
-		num_of_samples = n/(4*k)
+		num_of_samples = 50
 
+	#elif n/(k) > 50:
+	#	num_of_samples = 50
+	#
+	#elif n/(k) < 10:
+	#	num_of_samples = 10
+	#else:
+	#	num_of_samples = int(n/(4*k))
 
+	unique_X = np.unique(X, axis=0)
 	neigh = NearestNeighbors(num_of_samples)
-	neigh.fit(X)
 
-		
-	result = neigh.kneighbors(X, num_of_samples, return_distance=False)
-	import pdb; pdb.set_trace()
+	neigh.fit(unique_X)
+	
+	[dis, idx] = neigh.kneighbors(X, num_of_samples, return_distance=True)
+	dis_inv = 1/dis[:,1:]
+	idx = idx[:,1:]
+	
+
+	total_dis = np.sum(dis_inv, axis=1)
+	total_dis = np.reshape(total_dis,(n, 1))
+	total_dis = np.matlib.repmat(total_dis, 1, num_of_samples-1)
+	dis_ratios = dis_inv/total_dis
+
+	result_store_dictionary = {}
+	σ_list = np.zeros((n,1))
+	
+	for i in range(n):
+		if str(X[i,:]) in result_store_dictionary:
+			σ = result_store_dictionary[str(X[i,:])] 
+			σ_list[i] = σ
+			continue
+
+		dr = dis_ratios[i,:]
+
+		Δ = unique_X[idx[i,:],:] - X[i,:]
+		Δ2 = Δ*Δ
+		d = np.sum(Δ2,axis=1)
+		σ = np.sqrt(np.sum(dr*d))
+		σ_list[i] = σ#*10
+
+		result_store_dictionary[str(X[i,:])] = σ
+
+	#print(σ_list)
+	#import pdb; pdb.set_trace()
+	σ2 = σ_list.dot(σ_list.T)
+	
 
 
 
 	#n = X.shape[0]
-	#D = sklearn.metrics.pairwise.pairwise_distances(X, metric='euclidean', n_jobs=1)
+	D = sklearn.metrics.pairwise.pairwise_distances(X, metric='euclidean', n_jobs=1)
 	#sD = np.sort(D, axis=1)
 
 	#print(sD[:,1:20])
@@ -103,7 +141,7 @@ def STSC(X, k):
 	#import pdb; pdb.set_trace()
 	#σ_col = σ_col + 0.00001			# add noise in case of 0
 
-	σ2 = σ_col.dot(σ_col.T)
+	#σ2 = σ_col.dot(σ_col.T)
 
 	K = np.exp(-(D*D)/σ2)
 	np.fill_diagonal(K,0)
@@ -120,14 +158,15 @@ def STSC(X, k):
 
 
 
-#X = genfromtxt('../dataset/smiley.csv', delimiter=',')					# k = 3
-#X = genfromtxt('../dataset/spiral_arm.csv', delimiter=',')				# k = 3
-#X = genfromtxt('../dataset/inner_rings.csv', delimiter=',')				# k = 3
-#X = genfromtxt('../dataset/noisy_two_clusters.csv', delimiter=',')				# k = 3
-#X = genfromtxt('../dataset/four_lines.csv', delimiter=',')				# k = 4
+#X = genfromtxt('../dataset/smiley.csv', delimiter=','); k = 3
+#X = genfromtxt('../dataset/spiral_arm.csv', delimiter=','); k = 3
+#X = genfromtxt('../dataset/inner_rings.csv', delimiter=','); k = 3
+#X = genfromtxt('../dataset/noisy_two_clusters.csv', delimiter=','); k = 3
+#X = genfromtxt('../dataset/four_lines.csv', delimiter=','); k = 4
+
 #X = preprocessing.scale(X)
 #
-#labels = STSC(X,4)
+#labels = STSC(X,k)
 #cluster_plot(X, labels)
 
 
@@ -135,15 +174,16 @@ def STSC(X, k):
 
 
 
-X = genfromtxt('../dataset/breast-cancer.csv', delimiter=',')	# k = 2
-Y = genfromtxt('../dataset/breast-cancer-labels.csv', delimiter=',')	# k = 2
-
-#X = genfromtxt('../dataset/wine.csv', delimiter=',')	# k = 2
+#X = genfromtxt('../dataset/facial_85.csv', delimiter=','); k = 20
+#Y = genfromtxt('../dataset/facial_true_labels_624x960.csv', delimiter=','); 
+#X = genfromtxt('../dataset/breast-cancer.csv', delimiter=','); k = 2
+#Y = genfromtxt('../dataset/breast-cancer-labels.csv', delimiter=',')	# k = 2
+#X = genfromtxt('../dataset/wine.csv', delimiter=','); k = 3
 #Y = genfromtxt('../dataset/wine_label.csv', delimiter=',')	# k = 2
 
 X = preprocessing.scale(X)
-labels = STSC(X,2)
-#labels = spectral(X, 1.0, 2)
+labels = STSC(X,k)
+#labels = spectral(X, 9.0, 20)
 
 print(labels)
 nmi = normalized_mutual_info_score(labels, Y)
