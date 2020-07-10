dd69799ce3f96bc3f180d648fc11f54f90636ce1
chieh <juliusctw@gmail.com>
2020-07-10 16:44:37 -0400

    kinda working

--- sparse_eigen_decomposition/sparse_pca.py
+++ sparse_eigen_decomposition/sparse_pca.py
@@ -5,9 +5,10 @@ import numpy as np
 import sklearn.metrics
 import torch
 import numpy.matlib
-from torch.autograd import Variable
 from numpy.random import rand
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 np.set_printoptions(precision=3)
 np.set_printoptions(threshold=30)
@@ -21,13 +22,13 @@ class sparse_pca:
 		self.Kᵭ_threshold = 0.035 		# kernel divergence threshold
 		self.debug_record = {}
 
-	def subsample_matrix_with_dim(self, A, num_of_eigs, eigs_1, λ_id, ń): #A is square, n is the number we keep
+	def subsample_matrix_with_dim(self, A, eigs_1, λ_id, ń): #A is square, n is the number we keep
 		N = A.shape[0]
 		inc = 1
 		Kᵭ = 10
 		eigs_2 = 0
 
-		for m in range(400):
+		for ń in np.arange(ń, N, inc):
 			new_arrangement = np.random.permutation(N)
 			keep_index_better = np.sort(new_arrangement[0:ń])
 			discard_index_better = np.sort(new_arrangement[ń:])
@@ -39,75 +40,22 @@ class sparse_pca:
 			eigs_2_better = D[-1]
 			v2_better = V[:,-1]
 			
-			Kᵭ_better = np.linalg.norm(eigs_1 - eigs_2_better, ord=np.inf)
 			if eigs_2_better > eigs_2:
-				Kᵭ = Kᵭ_better
 				Aᵴ = Aᵴ_better
 				v2 = v2_better
 				keep_index = keep_index_better
 				eigs_2 = eigs_2_better
 				discard_index = discard_index_better
-				#print(eigs_2)
+
+				#print(eigs_1, eigs_2)
+				if eigs_1*0.99 < eigs_2:
+					print('\tEigen percentage : %.3f,  %.3f/%.3f'% (eigs_2/eigs_1, eigs_2, eigs_1))
+					print('\tSamples used : %d'% (ń))
+					break
+					#import pdb; pdb.set_trace()
 
 		self.debug_record[λ_id][Kᵭ] = Kᵭ
 		return [Aᵴ, v2, keep_index, discard_index]
-
-
-
-
-	def subsample_matrix(self, A, num_of_eigs, eigs_1, λ_id): #A is square, n is the number we keep
-		N = A.shape[0]
-		inc = 1
-
-		for ń in np.arange(num_of_eigs, A.shape[0]+1, inc):
-			new_arrangement = np.random.permutation(N)
-			keep_index = np.sort(new_arrangement[0:ń])
-			discard_index = np.sort(new_arrangement[ń:])
-			
-			B = A[keep_index, :]
-			Aᵴ = B[:, keep_index]
-		
-			[ksf_2, eigs_2, num_of_eigs, v2, keepK_num] = self.get_kernel_sampling_feature(Aᵴ, num_of_eigs)
-			
-			if len(eigs_1) > len(eigs_2):	
-				extra_pad = len(eigs_1) - len(eigs_2)
-				eigs_2 = np.pad(eigs_2, (0,extra_pad), 'constant')
-				
-			Kᵭ = np.linalg.norm(eigs_1 - eigs_2, ord=np.inf)
-			if Kᵭ < self.Kᵭ_threshold: 
-				for m in range(20):
-
-					new_arrangement = np.random.permutation(N)
-					keep_index_better = np.sort(new_arrangement[0:ń])
-					discard_index_better = np.sort(new_arrangement[ń:])
-		
-					B = A[keep_index_better, :]
-					Aᵴ_better = B[:, keep_index_better]
-				
-					[ksf_2_better, eigs_2_better, num_of_eigs_better, v2_better, keepK_num] = self.get_kernel_sampling_feature(Aᵴ_better, num_of_eigs)
-					
-					if len(eigs_1) > len(eigs_2_better):	
-						extra_pad = len(eigs_1) - len(eigs_2_better)
-						eigs_2_better = np.pad(eigs_2_better, (0,extra_pad), 'constant')
-						
-					Kᵭ_better = np.linalg.norm(eigs_1 - eigs_2_better, ord=np.inf)
-					if Kᵭ_better < Kᵭ:
-						Kᵭ = Kᵭ_better
-						Aᵴ = Aᵴ_better
-						v2 = v2_better
-						keep_index = keep_index_better
-						discard_index = discard_index_better
-
-				self.debug_record[λ_id][Kᵭ] = Kᵭ
-				return [Aᵴ, v2, keep_index, discard_index]
-
-
-
-
-	def double_center(self, Ψ):
-		HΨ = Ψ - np.mean(Ψ, axis=0)								# equivalent to Γ = Ⲏ.dot(Kᵧ).dot(Ⲏ)
-		HΨH = (HΨ.T - np.mean(HΨ.T, axis=0)).T
-		return HΨH
 
 
 	def get_kernel_sampling_feature(self, rbk, num_of_eigs=None):
@@ -116,6 +64,7 @@ class sparse_pca:
 		v = V[0,:]	# most dominant eig
 		sorted_v = np.flip(np.sort(np.absolute(v)))
 		keepK_num = np.sum(np.cumsum(sorted_v/np.sum(sorted_v)) < self.sparcity)
+		print('\t', keepK_num)
 
 		cs = np.cumsum(S)/np.sum(S)
 		L1_norm = S/np.sum(S)
@@ -149,7 +98,7 @@ class sparse_pca:
 		print('\t vAᵴv : %.3f'%self.debug_record[λ_id]['vAᵴv'])
 		print('\t ῡAῡ : %.3f'%self.debug_record[λ_id]['ῡAῡ'])
 
-	def get_sparce_eig_vector(self, A, num_of_eigs, sparcity=0.9):
+	def get_sparce_eig_vector(self, A, num_of_eigs=2, sparcity=0.95):
 		orig_A = A.copy()
 		self.sparcity = sparcity		# 0 to 1, the closer to 0 the more sparse
 		n = A.shape[0]
@@ -159,8 +108,17 @@ class sparse_pca:
 			print('Layer : %d'%λ_id)
 			self.debug_record[λ_id] = {}
 	
-			[keepCS, eigs_1, num_of_eigs, v1, keepK_num] = self.get_kernel_sampling_feature(A, 1)
-			[Aᵴ, v2, keep_index, discard_index] = self.subsample_matrix_with_dim(A, num_of_eigs, eigs_1, λ_id, keepK_num)
+			#[keepCS, eigs_1, num_of_eigs, v1, keepK_num] = self.get_kernel_sampling_feature(A, 1)
+
+
+			[D,V] = np.linalg.eigh(A)
+			eigs_1 = D[-1]
+			v1 = V[:,-1]
+			sorted_v = np.flip(np.sort(np.absolute(v1)))
+			keepK_num = np.sum(np.cumsum(sorted_v/np.sum(sorted_v)) < self.sparcity)
+			
+
+			[Aᵴ, v2, keep_index, discard_index] = self.subsample_matrix_with_dim(A, eigs_1, λ_id, keepK_num)
 
 			vi = self.merge_eig_vectors(v1,v2, A, Aᵴ, keep_index, discard_index, λ_id)
 			vi = np.reshape(vi, (len(vi), 1))
@@ -178,26 +136,53 @@ class sparse_pca:
 		D = (np.diag(eigvec.T.dot(orig_A).dot(eigvec)))
 		return [D, eigvec]
 
-
-#x = np.vstack((np.random.randn(30,2), np.random.randn(30,2) + 5))
+#	Data 1
+x = np.vstack((np.random.randn(30,5), np.random.randn(30,5) + 10))
+x = preprocessing.scale(x)
+γ = 1.0/(2*0.5*0.5)
+A = sklearn.metrics.pairwise.rbf_kernel(x, gamma=γ)
+np.fill_diagonal(A, 0)			#	Set diagonal of adjacency matrix to 0
 #A = x.dot(x.T)
+#Dinv = np.diag(1/np.sum(A,axis=1))
+#A = Dinv.dot(A)
+#import pdb; pdb.set_trace()
+
+##	Data 2
 #A = np.random.randn(100,100)
 #A = A.dot(A.T)
-A =  np.array([[ 5.95 ,  2.558, -0.045,  5.369,  0.571,  4.871, -1.672,  0.784, -1.175, -2.351],
-				[ 2.558,  9.927, -1.97 ,  1.867,  8.123,  1.998, -3.867, -5.423, -4.889, -3.455],
-				[-0.045, -1.97 , 11.314,  3.922, -3.707,  2.685,  5.36 , -4.995, -0.136,  1.396],
-				[ 5.369,  1.867,  3.922,  8.214,  0.626,  3.814, -0.26 , -2.162,  1.232, -1.246],
-				[ 0.571,  8.123, -3.707,  0.626, 12.372, -0.182, -2.772, -1.065, -2.313,  0.384],
-				[ 4.871,  1.998,  2.685,  3.814, -0.182,  6.045,  0.541,  0.586, -3.12 , -1.502],
-				[-1.672, -3.867,  5.36 , -0.26 , -2.772,  0.541,  4.191,  0.548,  0.72 ,  2.633],
-				[ 0.784, -5.423, -4.995, -2.162, -1.065,  0.586,  0.548, 10.719,  2.188,  2.767],
-				[-1.175, -4.889, -0.136,  1.232, -2.313, -3.12 ,  0.72 ,  2.188,  5.608,  2.17 ],
-				[-2.351, -3.455,  1.396, -1.246,  0.384, -1.502,  2.633,  2.767,  2.17 ,  3.465]])
 
-#[D,V] = np.linalg.eigh(A)
-#print(np.flip(np.sort(D)))
+##	Data 3
+#A =  np.array([[ 5.95 ,  2.558, -0.045,  5.369,  0.571,  4.871, -1.672,  0.784, -1.175, -2.351],
+#				[ 2.558,  9.927, -1.97 ,  1.867,  8.123,  1.998, -3.867, -5.423, -4.889, -3.455],
+#				[-0.045, -1.97 , 11.314,  3.922, -3.707,  2.685,  5.36 , -4.995, -0.136,  1.396],
+#				[ 5.369,  1.867,  3.922,  8.214,  0.626,  3.814, -0.26 , -2.162,  1.232, -1.246],
+#				[ 0.571,  8.123, -3.707,  0.626, 12.372, -0.182, -2.772, -1.065, -2.313,  0.384],
+#				[ 4.871,  1.998,  2.685,  3.814, -0.182,  6.045,  0.541,  0.586, -3.12 , -1.502],
+#				[-1.672, -3.867,  5.36 , -0.26 , -2.772,  0.541,  4.191,  0.548,  0.72 ,  2.633],
+#				[ 0.784, -5.423, -4.995, -2.162, -1.065,  0.586,  0.548, 10.719,  2.188,  2.767],
+#				[-1.175, -4.889, -0.136,  1.232, -2.313, -3.12 ,  0.72 ,  2.188,  5.608,  2.17 ],
+#				[-2.351, -3.455,  1.396, -1.246,  0.384, -1.502,  2.633,  2.767,  2.17 ,  3.465]])
+
+
+#	Input matrix A must be SPSD
+eigN = 2
+
 sp = sparse_pca()
-[D,V] = sp.get_sparce_eig_vector(A,5, sparcity=0.7)		# 0 to 1, the closer to 0 the more sparse
+[D,V] = sp.get_sparce_eig_vector(A, num_of_eigs=eigN)		# sparcity : 0 to 1, the closer to 0 the more sparse
+[D2,V2] = np.linalg.eigh(A)
+
+print('\nOutput')
+print('Actual Eigen values : ', np.flip(D2)[0:eigN])
 print('Eigen values : ', D)
 print('Eigen vector : ')
-print(V)
+print(V[0:30,:], '\n')
+print(V[30:60,:], '\n')
+
+[V,D] = np.linalg.eigh(V.T.dot(V))
+
+import pdb; pdb.set_trace()
+fig = plt.figure()
+plt.plot(V[0:30,0], V[0:30,1],'or')
+plt.plot(V[30:60,0], V[30:60,1],'ob')
+plt.show()
+
