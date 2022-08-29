25f025d96abb7acb58a9a8171706ef38c03d37fe
endsley <chieh.t.wu@gmail.com>
2022-08-29 09:28:03 -0400

    kk

--- kernel_eigVector_as_eigFunctions/K_EigV_to_eigFunc.py
+++ kernel_eigVector_as_eigFunctions/K_EigV_to_eigFunc.py
@@ -17,13 +17,13 @@ np.set_printoptions(threshold=sys.maxsize)
 #	ONLEARNING WITHINTEGRALOPERATORS
 #	The idea is that we know what the relationship between eigenvectors of the kernel matrix and the eigen functions.
 
-#	Given $\phi_i$ as the feature map and $n$ as the number of samples, we define the integral operator $T_n$ as 
-#	$$T_n = \frac{1}{n}\sum_i \phi_i(x_i) \phi_i^T(x_i) $$
+#	Given $\psi_i$ as the feature map and $n$ as the number of samples, we define the integral operator $T_n$ as 
+#	$$T_n = \frac{1}{n}\sum_i \psi_i(x_i) \psi_i^T(x_i) $$
 
 #	In this experiment, we will use  the feature map of a polynomial kernel 
 #	$k(x,y) = (x^Ty + 1)^2$
 #	We will assume that each sample has two dimensions, therefore the feature map is finite 
-#	$\phi(x) = [x_1^2,  \sqrt{2} x_1x_2, \sqrt{2} x_1, \sqrt{2} x_2, x_2^2, 1]$
+#	$\psi(x) = [x_1^2,  \sqrt{2} x_1x_2, \sqrt{2} x_1, \sqrt{2} x_2, x_2^2, 1]$
 #	By using this equation, we can compute Tn, the approximate integral operator. 
 
 
@@ -36,13 +36,13 @@ def polyK(X):	# calculate the polynomial kernel matrix
 			K[i,j] = (xi.T.dot(yi) + 1)**2
 	return K
 
-def Φ(X):		# this function takes x and calculate its feature map
+def Ψ(X):		# this function takes x and calculate its feature map
 	fm = np.empty((6, 0))
 	c = np.sqrt(2)
 	for x in X:
 		# feature map of polynomial [x₁ᒾ,  ᴄ x₁x₂, ᴄ x₁, c x₂, x₂ᒾ, 1]
-		φ = np.array([[x[0]*x[0], c*x[0]*x[1], c*x[0], c*x[1], x[1]*x[1], 1]]).T
-		fm = np.hstack((fm, φ))
+		ψ = np.array([[x[0]*x[0], c*x[0]*x[1], c*x[0], c*x[1], x[1]*x[1], 1]]).T
+		fm = np.hstack((fm, ψ))
 	return fm.T
 
 
@@ -53,10 +53,10 @@ if __name__ == "__main__":
 	X = np.array([[1,1], [2,2], [3,3]])
 	n = X.shape[0]
 	#	
-	Φ = Φ(X)						# feature map
+	Ψ = Ψ(X)						# feature map
 	K = polyK(X)					# X to kernel matrix
 	#
-	Tn = (1/n)*Φ.T.dot(Φ)			# The approximate integral operator
+	Tn = (1/n)*Ψ.T.dot(Ψ)			# The approximate integral operator
 	#
 	[λ, V] = eig(K)				# eigenvalue/vector of the kernel matrix
 	[σ, U] = eig(Tn)				# eigenvalue/function of the integral operator
@@ -88,45 +88,52 @@ if __name__ == "__main__":
 	block_two_string_concatenate(eigK, eigQ, spacing='\t', add_titles=[], auto_print=True)
 #
 
-#	Given $f_i$ as the $i_{th}$ eigenfunction associated with the eigenvector of $K$ denoted as $v_i$.
+#	Given $\phi_i$ as the $i_{th}$ eigenfunction associated with the eigenvector of $K$ denoted as $v_i$.
 #	Given $\sigma_i$ as the eigenvalues of the integral operator (not from the kernel)
 #	Note that the relationship between the eigenvectors of K and eigenfunction is
-#	$$f_i = \frac{1}{\sqrt{n \sigma_i}} \Phi^T v_i$$
+#	$$\phi_i = \frac{1}{\sqrt{n \sigma_i}} \Psi^T v_i$$
 
 #	where
-#	$$\Phi^T = \begin{bmatrix} \phi(x_1) &  \phi(x_2) & ... & \phi(x_n) \end{bmatrix}$$
+#	$$\Psi^T = \begin{bmatrix} \psi(x_1) &  \psi(x_2) & ... & \psi(x_n) \end{bmatrix}$$
 
 #	Notice that our eigenvector eigenfunction relationship is slightly different from the equation proposed by 
 #	Rosasco, and that is because he has defined the kernel matrix differently. 
 
 	σ3 = σ[0:3]
-	eigFun_from__eigV_of_K = (1/np.sqrt(n*σ3))*((Φ.T.dot(V)))
-	eigFun = U[:,0:3]
+	eigFun_from__eigV_of_K = (1/np.sqrt(n*σ3))*((Ψ.T.dot(V)))
+	ϕ = U[:,0:3]
 #
 	jprint('Notice how the eigenfunction computed from the eigenvectors is identical to the actual eigenfunctions !!!')
 	eigFun_from__eigV_of_K = pretty_np_array(eigFun_from__eigV_of_K, front_tab='', title='Computed eig Function', auto_print=False)
-	eigFun = pretty_np_array(eigFun, front_tab='', title='True Eigen Function', auto_print=False)
-	block_two_string_concatenate(eigFun_from__eigV_of_K, eigFun, spacing='\t', add_titles=[], auto_print=True)
+	eigFun_str = pretty_np_array(ϕ, front_tab='', title='True Eigen Function', auto_print=False)
+	block_two_string_concatenate(eigFun_from__eigV_of_K, eigFun_str, spacing='\t', add_titles=[], auto_print=True)
 
 #	Alternatively, if we let $\lambda_i$ be the eigenvalues from the kernel matrix K, then the relationship would be
-#	$$f_i = \frac{1}{\sqrt{\lambda_i}} \Phi^T v_i$$
+#	$$\psi_i = \frac{1}{\sqrt{\lambda_i}} \Psi^T v_i$$
 
-	eigFun_from__eigV_of_K = (1/np.sqrt(λ))*((Φ.T.dot(V)))
-	eigFun = U[:,0:3]
+	eigFun_from__eigV_of_K = (1/np.sqrt(λ))*((Ψ.T.dot(V)))
+	ϕ = U[:,0:3]
 #
 	jprint('Notice how the eigenfunction computed from the eigenvectors is identical to the actual eigenfunctions !!!')
 	eigFun_from__eigV_of_K = pretty_np_array(eigFun_from__eigV_of_K, front_tab='', title='Computed eig Function', auto_print=False)
-	eigFun = pretty_np_array(eigFun, front_tab='', title='True Eigen Function', auto_print=False)
-	block_two_string_concatenate(eigFun_from__eigV_of_K, eigFun, spacing='\t', add_titles=[], auto_print=True)
+	eigFun_str = pretty_np_array(ϕ, front_tab='', title='True Eigen Function', auto_print=False)
+	block_two_string_concatenate(eigFun_from__eigV_of_K, eigFun_str, spacing='\t', add_titles=[], auto_print=True)
 
 
 
 #	Next, we can get the inverse relationship where we go from the eigenfunction back to the eigenvector. 
 #	We use the following derivation
-#	$$f_i = \frac{1}{\sqrt{\lambda_i}} \Phi^T v_i$$
-#	$$\Phi f_i = \frac{1}{\sqrt{\lambda_i}} \Phi \Phi^T v_i$$
-#	$$\Phi f_i = \frac{1}{\sqrt{\lambda_i}} K v_i$$
-#	$$\Phi f_i = \frac{\lambda_i}{\sqrt{\lambda_i}} v_i$$
-#	$$\Phi f_i = \sqrt{\lambda_i} v_i$$
-#	$$\frac{1}{\sqrt{\lambda_i}}\Phi f_i = v_i$$
-#	$$\frac{1}{\sqrt{\lambda_i}}\Phi \begin{bmatrix} f_1 & f_2 & .. \end{bmatrix} = \begin{bmatrix} v_1 & v_2 & .. \end{bmatrix}$$
+#	$$\phi_i = \frac{1}{\sqrt{\lambda_i}} \Psi^T v_i$$
+#	$$\Psi \phi_i = \frac{1}{\sqrt{\lambda_i}} \Psi \Psi^T v_i$$
+#	$$\Psi \phi_i = \frac{1}{\sqrt{\lambda_i}} K v_i$$
+#	$$\Psi \phi_i = \frac{\lambda_i}{\sqrt{\lambda_i}} v_i$$
+#	$$\Psi \phi_i = \sqrt{\lambda_i} v_i$$
+#	$$\frac{1}{\sqrt{\lambda_i}}\Psi \phi_i = v_i$$
+#	$$\frac{1}{\sqrt{\lambda_i}}\Psi \begin{bmatrix} f_1 & f_2 & .. \end{bmatrix} = \begin{bmatrix} v_1 & v_2 & .. \end{bmatrix}$$
+
+	
+	original_u = (1/np.sqrt(λ))*((Ψ.dot(ϕ)))
+	computedV = pretty_np_array(original_u, front_tab='', title='Computed eigvector', auto_print=False)
+	actualV = pretty_np_array(V, front_tab='', title='True Eigenvector', auto_print=False)
+	block_two_string_concatenate(computedV, actualV, spacing='\t', add_titles=[], auto_print=True)
+
