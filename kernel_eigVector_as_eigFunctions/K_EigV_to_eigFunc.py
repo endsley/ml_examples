ec4dd0137eeab61da723e14e59da927fdadfd666
endsley <chieh.t.wu@gmail.com>
2022-08-28 20:57:35 -0400

    kk

--- kernel_eigVector_as_eigFunctions/K_EigV_to_eigFunc.py
+++ kernel_eigVector_as_eigFunctions/K_EigV_to_eigFunc.py
@@ -17,11 +17,14 @@ np.set_printoptions(threshold=sys.maxsize)
 #	ONLEARNING WITHINTEGRALOPERATORS
 #	The idea is that we know what the relationship between eigenvectors of the kernel matrix and the eigen functions.
 
-#	Given $\phi_i$ as the eigenfunctions and $m$ as the number of eigenfunctions, we define the integral operator $T_n$ as 
-#	$$T_n = \frac{1}{m}\sum_i \phi_i \phi_i^T $$
+#	Given $\phi_i$ as the feature map and $n$ as the number of samples, we define the integral operator $T_n$ as 
+#	$$T_n = \frac{1}{n}\sum_i \phi_i(x_i) \phi_i^T(x_i) $$
 
-#	In this experiment, we will use  the feature map of a polynomial kernel
-#	Since the feature map is finite [x₁ᒾ,  ᴄ x₁x₂, ᴄ x₁, c x₂, x₂ᒾ, 1], we can compute Tn, the approximate integral operator. 
+#	In this experiment, we will use  the feature map of a polynomial kernel 
+#	$k(x,y) = (x^Ty + 1)^2$
+#	We will assume that each sample has two dimensions, therefore the feature map is finite 
+#	$\phi(x) = [x_1^2,  \sqrt{2} x_1x_2, \sqrt{2} x_1, \sqrt{2} x_2, x_2^2, 1]$
+#	By using this equation, we can compute Tn, the approximate integral operator. 
 
 
 def polyK(X):	# calculate the polynomial kernel matrix
@@ -33,7 +36,7 @@ def polyK(X):	# calculate the polynomial kernel matrix
 			K[i,j] = (xi.T.dot(yi) + 1)**2
 	return K
 
-def Φ(X):
+def Φ(X):		# this function takes x and calculate its feature map
 	fm = np.empty((6, 0))
 	c = np.sqrt(2)
 	for x in X:
@@ -64,10 +67,9 @@ if __name__ == "__main__":
 	eK = pretty_np_array(λ, front_tab='', title='Eig Values of K', auto_print=False)
 	eQ = pretty_np_array(σ, front_tab='', title='Eig Values of Tn', auto_print=False)
 	block_two_string_concatenate(eK, eQ, spacing='\t', add_titles=[], auto_print=True)
-	#
-	jprint('Notice that the eigenvectors of K is not the same as Tn!!!')
-	jprint('However if we multiply the eigenvalues by n, they become the same.\n')
-#
+	
+	#Notice that the eigenvectors of K is not the same as $T_n$!!!
+	#However if we multiply the eigenvalues by n, they become the same.
 #
 	nσ = n*σ[0:3]
 	σ_text = pretty_np_array(nσ, front_tab='', title='Eigenvalues after multiplied by n', auto_print=False, end_space=" = 3 * ") 
@@ -75,11 +77,12 @@ if __name__ == "__main__":
 	display1 = block_two_string_concatenate(σ_text, eQn, spacing=' ', add_titles=['Eigs'], auto_print=False)
 	jprint(display1)
 
+#	Therefore, we conclude the relationship between eigenvalues of kernel matrix and $T_n$ is
+#	$$\mathbf{\sigma(K) = n*\sigma(T_n)}$$
 
+#	We next verify the eigenvector eigenfunction relationship.
+#	We first show the eigenvectors of K and eigenfunctions from Tn
 
-	jprint('Therefore, we conclude that σ(K) = n*σ(Tn), we next verify the eigenvector eigenfunction relationship.\n')
-	jprint('We first show the eigenvectors of K and eigenfunctions from Tn.')
-#
 	eigK = pretty_np_array(V, front_tab='', title='Eig of K', auto_print=False)
 	eigQ = pretty_np_array(U, front_tab='', title='Eig of Tn', auto_print=False)
 	block_two_string_concatenate(eigK, eigQ, spacing='\t', add_titles=[], auto_print=True)
@@ -93,6 +96,8 @@ if __name__ == "__main__":
 #	where
 #	$$\Phi^T = \begin{bmatrix} \phi(x_1) &  \phi(x_2) & ... & \phi(x_n) \end{bmatrix}$$
 
+#	Notice that our eigenvector eigenfunction relationship is slightly different from the equation proposed by 
+#	Rosasco, and that is because he has defined the kernel matrix differently. 
 
 	σ3 = σ[0:3]
 	eigFun_from__eigV_of_K = (1/np.sqrt(n*σ3))*((Φ.T.dot(V)))
@@ -114,3 +119,14 @@ if __name__ == "__main__":
 	eigFun = pretty_np_array(eigFun, front_tab='', title='True Eigen Function', auto_print=False)
 	block_two_string_concatenate(eigFun_from__eigV_of_K, eigFun, spacing='\t', add_titles=[], auto_print=True)
 
+
+
+#	Next, we can get the inverse relationship where we go from the eigenfunction back to the eigenvector. 
+#	We use the following derivation
+#	$$f_i = \frac{1}{\sqrt{\lambda_i}} \Phi^T v_i$$
+#	$$\Phi f_i = \frac{1}{\sqrt{\lambda_i}} \Phi \Phi^T v_i$$
+#	$$\Phi f_i = \frac{1}{\sqrt{\lambda_i}} K v_i$$
+#	$$\Phi f_i = \frac{\lambda_i}{\sqrt{\lambda_i}} v_i$$
+#	$$\Phi f_i = \sqrt{\lambda_i} v_i$$
+#	$$\frac{1}{\sqrt{\lambda_i}}\Phi f_i = v_i$$
+#	$$\frac{1}{\sqrt{\lambda_i}}\Phi \begin{bmatrix} f_1 & f_2 & .. \end{bmatrix} = \begin{bmatrix} v_1 & v_2 & .. \end{bmatrix}$$
