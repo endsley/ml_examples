75844b98ed3195f7aab4ef0c87044c2732e9bd5a
endsley <chieh.t.wu@gmail.com>
2024-11-13 16:42:03 -0500

    added LDA code

---
+++ LDA/separate_centers.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import numpy as np
+from numpy import mean, reshape, array
+from numpy import vstack, hstack
+from matplotlib import pyplot as plt
+
+
+X0 = array([	[-2,1],
+				[0,3],
+				[1,4],
+				[1,5],
+				[2,5]])
+
+X1 = array([	[0,0],
+				[1,1],
+				[2,1],
+				[3,2],
+				[4,3]])
+
+X = vstack((X0, X1))
+
+# 	Solving LDA
+v = reshape(mean(X0, axis=0) - mean(X1, axis=0), (2,1))
+S1 = v.dot(v.T)
+[D,V] = np.linalg.eigh(S1)
+
+#import pdb; pdb.set_trace()
+
+ṽւ = reshape(V[:,1], (2,1))
+linePoints = vstack((5*ṽւ.T, -5*ṽւ.T))
+
+#	Project the data onto the LDA line
+X0ᴾ = (X0 ).dot(ṽւ).dot(ṽւ.T)		# data after LDA projection
+X1ᴾ = (X1 ).dot(ṽւ).dot(ṽւ.T)		# data after LDA projection
+
+plt.figure(figsize = (5, 5))
+plt.plot(linePoints[:,0], linePoints[:,1], color='blue')
+plt.scatter(X0[:,0], X0[:,1], color='red', s=16)
+plt.scatter(X0ᴾ[:,0], X0ᴾ[:,1], color='red', s=16)
+plt.scatter(X1[:,0], X1[:,1], color='green', s=16)
+plt.scatter(X1ᴾ[:,0], X1ᴾ[:,1], color='green', s=16)
+plt.xlim([-3,6])
+plt.ylim([-3,6])
+plt.title('Original Points vs Projected Points')
+plt.tight_layout()
+plt.show()
+
