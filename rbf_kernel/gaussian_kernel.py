cdd26b179a0151862935318e9beb87e3c2083f83
endsley <chieh.t.wu@gmail.com>
2024-11-20 18:29:46 -0500

    kk

--- rbf_kernel/gaussian_kernel.py
+++ rbf_kernel/gaussian_kernel.py
@@ -1,3 +1,28 @@
-#!/usr
-import sklearn
-sklearn.metrics.pairwise.rbf_kernel(X, Y=None, gamma=None)
+#!/usr/bin/env python
+
+from sklearn.metrics.pairwise import rbf_kernel
+import numpy as np
+from numpy import exp as e
+from numpy import zeros, array, reshape
+
+#	Write my own kernel
+X = array([[1,2],[0,1],[1,0]])
+
+def k(xi, xj):
+	γ = 1
+	d = (xi - xj).T.dot((xi - xj))
+	return e(-γ*d)
+
+K = zeros((3,3))
+for i, xi in enumerate(X):
+	for j, xj in enumerate(X):
+		xi = reshape(xi, (2,1))
+		xj = reshape(xj, (2,1))
+		K[i,j] = k(xi, xj)
+
+print(K)
+
+#	Use sklearn to generate the kernel
+#	Notice it is the same as calculated by the equation
+K = rbf_kernel(X, gamma=1)
+print(K)
